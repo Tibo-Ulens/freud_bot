44dcb98788e12ee9705e0e628392c880aed3f358
Tibo Ulens <ulens.tibo@gmail.com>
2022-10-08 18:15:15 +0200

    add mommy command (sigh)

---
+++ bot/extensions/random.py
@@ -0,0 +1,22 @@
+from discord import app_commands, Interaction
+from discord.ext.commands import Cog
+
+from bot.bot import Bot
+
+
+class Random(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @app_commands.command(
+        name="mommy",
+        description="For when somebody likes their mother a little *too* much",
+    )
+    async def mommy(self, iactn: Interaction):
+        await iactn.response.send_message(
+            "The Oedipus complex is a phase in the life of a young boy in which he wishes to have sex with his mother"
+        )
+
+
+async def setup(bot: Bot):
+    await bot.add_cog(Random(bot))
