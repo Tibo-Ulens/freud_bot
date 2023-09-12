9aa7f876d1055a6234df2704afab5c28c7d488f6
Tibo Ulens <ulens.tibo@gmail.com>
2023-09-12 15:45:49 +0200

    feat(bot): add russian roulette confessions

--- bot/extensions/fun/confess.py
+++ bot/extensions/fun/confess.py
@@ -1,9 +1,20 @@
+import random
+
 import discord
-from discord import app_commands, Interaction, ButtonStyle, TextChannel, Embed, Colour
+from discord import (
+    app_commands,
+    Interaction,
+    ButtonStyle,
+    TextChannel,
+    Embed,
+    Colour,
+    Member,
+)
 from discord.ui import View, Button
 
 from models.config import Config
 
+from bot import util
 from bot.bot import Bot
 from bot.decorators import (
     check_has_config_option,
@@ -12,14 +23,42 @@ from bot.decorators import (
 from bot.extensions import ErrorHandledCog
 
 
+async def send_pending_confession(
+    confession: str,
+    confession_channel: TextChannel,
+    approval_channel: TextChannel,
+    russian_roulette_user: Member = None,
+):
+    confession_embed = Embed(description=confession, colour=Colour.from_str("#88a8bf"))
+    pending_view = PendingApprovalView(
+        confession=confession_embed,
+        confession_channel=confession_channel,
+        russian_roulette_user=russian_roulette_user,
+    )
+
+    pending_view.message = await approval_channel.send(
+        embed=confession_embed,
+        view=pending_view,
+    )
+
+
 class PendingApprovalView(View):
-    def __init__(self, confession: Embed, confession_channel: TextChannel):
+    def __init__(
+        self,
+        confession: Embed,
+        confession_channel: TextChannel,
+        russian_roulette_user: Member = None,
+    ):
         super().__init__(timeout=None)
         self.confession = confession
         self.confession_channel = confession_channel
+        self.russian_roulette_user = russian_roulette_user
 
     @discord.ui.button(label="✓", style=ButtonStyle.green)
     async def approve(self, ia: Interaction, btn: Button):
+        if self.russian_roulette_user is not None and random.random() <= 1 / 6:
+            self.confession.description = f"{util.render_user(self.russian_roulette_user)}: {self.confession.description}"
+
         await self.confession_channel.send(embed=self.confession)
 
         for item in self.children:
@@ -59,19 +98,38 @@ class Confess(ErrorHandledCog):
             ia.guild.channels, id=config.confession_channel
         )
 
-        confession_embed = Embed(
-            description=confession, colour=Colour.from_str("#88a8bf")
-        )
-        pending_view = PendingApprovalView(
-            confession=confession_embed, confession_channel=confession_channel
-        )
-
-        pending_view.message = await approval_channel.send(
-            embed=confession_embed,
-            view=pending_view,
-        )
+        await send_pending_confession(confession, confession_channel, approval_channel)
 
         await ia.response.send_message(content="Confession sent", ephemeral=True)
+
+    @app_commands.command(
+        name="russianconfess",
+        description="send confession with a 1 in 6 chance of not being anonymous",
+    )
+    @app_commands.describe(confession="The confession you want to post")
+    @app_commands.guild_only()
+    @check_has_config_option("confession_approval_channel")
+    @check_has_config_option("confession_channel")
+    @check_user_is_verified()
+    async def russian_confess(self, ia: Interaction, confession: str):
+        config = await Config.get(ia.guild_id)
+        approval_channel = discord.utils.get(
+            ia.guild.channels, id=config.confession_approval_channel
+        )
+        confession_channel = discord.utils.get(
+            ia.guild.channels, id=config.confession_channel
+        )
+
+        await send_pending_confession(
+            confession,
+            confession_channel,
+            approval_channel,
+            russian_roulette_user=ia.user,
+        )
+
+        await ia.response.send_message(
+            content="Russian roulette confession sent", ephemeral=True
+        )
 
 
 async def setup(bot: Bot):
