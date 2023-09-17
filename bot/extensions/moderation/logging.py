950d0e5f4e5e1e5cf06e88d8760104af48184a48
Tibo Ulens <ulens.tibo@gmail.com>
2023-09-17 21:57:36 +0200

    refactor(bot): remove info message for failed verifications

--- bot/extensions/moderation/logging.py
+++ bot/extensions/moderation/logging.py
@@ -1,7 +1,6 @@
 import logging
 
-import discord
-from discord import Message, Guild
+from discord import Guild
 
 from models.config import Config
 
@@ -13,16 +12,6 @@ from bot.log.guild_adapter import GuildAdapter
 
 
 logger = logging.getLogger("bot")
-
-
-MSG = """
-This does not seem to be a valid `/verify` command, did you use the autocomplete prompt?
-
-Normally you should get an autocomplete prompt when you start typing `/verify`.
-You can select this prompt by fully typing out `/verify` and then a space, by pressing tab, or by clicking on the prompt.
-
-If you experience further issues, please contact somebody that looks important.
-"""
 
 
 class ModLogging(ErrorHandledCog):
@@ -51,26 +40,6 @@ class ModLogging(ErrorHandledCog):
         logger.info(f"joined guild {util.render_guild(guild)}")
         await Config.create(guild_id=guild.id)
         logger.info(f"created config for guild {util.render_guild(guild)}")
-
-    @ErrorHandledCog.listener()
-    async def on_message(self, msg: Message):
-        if msg.author == self.bot.user:
-            return
-
-        guild_config = await Config.get(msg.guild.id)
-        if (
-            guild_config is not None
-            and guild_config.verification_channel is not None
-            and msg.channel.id == guild_config.verification_channel
-        ):
-            if (
-                guild_config.admin_role is not None
-                and discord.utils.get(msg.guild.roles, id=guild_config.admin_role)
-                in msg.author.roles
-            ):
-                return
-
-            await msg.reply(MSG)
 
 
 async def setup(bot: Bot):
