e0adefd82920de56577df53ab8217cffefaf4380
Tibo Ulens <ulens.tibo@gmail.com>
2023-09-22 16:01:59 +0200

    feat(bot): improve message scheduling feature

--- bot/extensions/utils/schedule_message.py
+++ bot/extensions/utils/schedule_message.py
@@ -1,11 +1,71 @@
 import asyncio
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from typing import Optional
 
-from discord import app_commands, Interaction, TextChannel
+from discord import app_commands, Interaction, TextChannel, TextStyle
+from discord.ui import Modal, TextInput
 
 from bot.bot import Bot
 from bot.extensions import ErrorHandledCog
+
+
+class ExpiredTimestamp(ValueError):
+    """Timestamp was in the past"""
+
+    def __init__(self, timestamp: str, delay: timedelta):
+        self.info = f"Error: message would have been sent in the past ({timestamp}, {delay} ago)"
+
+        super().__init__()
+
+
+def schedule_message(
+    loop, channel: TextChannel, parsed_time: str, message: str
+) -> timedelta:
+    now: datetime = datetime.now().astimezone(timezone.utc).replace(microsecond=0)
+    parsed_with_tz: datetime = parsed_time.astimezone(timezone.utc).replace(
+        microsecond=0
+    )
+
+    delay: timedelta = parsed_with_tz - now
+
+    if delay.total_seconds() < 0:
+        raise ExpiredTimestamp(parsed_time, -delay)
+
+    async def _schedule_message(channel: TextChannel, delay: float, message: str):
+        await asyncio.sleep(delay)
+        await channel.send(content=message)
+
+    loop.create_task(_schedule_message(channel, delay.total_seconds(), message))
+
+    return delay
+
+
+class MessagePrompt(Modal):
+    message = TextInput(label="message", style=TextStyle.paragraph)
+
+    def __init__(
+        self,
+        loop,
+        channel: TextChannel,
+        parsed_time: str,
+    ) -> None:
+        self.loop = loop
+        self.channel = channel
+        self.parsed_time = parsed_time
+
+        super().__init__(title="Message", timeout=None)
+
+    async def on_submit(self, ia: Interaction):
+        message = self.message.value
+
+        try:
+            delay = schedule_message(self.loop, self.channel, self.parsed_time, message)
+        except ExpiredTimestamp as err:
+            return await ia.response.send_message(err.info)
+
+        return await ia.response.send_message(
+            f"Message scheduled to be sent in {delay}, at {self.parsed_time}"
+        )
 
 
 class ScheduleMessage(ErrorHandledCog):
@@ -14,43 +74,45 @@ class ScheduleMessage(ErrorHandledCog):
 
         super().__init__(bot)
 
-    async def send_delayed_message(
-        self, channel: TextChannel, timestamp: datetime, message: str
-    ):
-        now = datetime.now()
-        delay = (timestamp - now).total_seconds()
-
-        self.bot.logger.info(f"message scheduled in {timestamp} - {now} = {delay}s")
-
-        await asyncio.sleep((timestamp - now).total_seconds())
-
-        await channel.send(content=message)
-
     @app_commands.command(
-        name="schedule", description="schedule a message to be sent at a specific time"
+        name="schedule", description="Schedule a message to be sent at a specific time"
     )
     @app_commands.describe(
-        time="the time to post the message at",
-        message="the message to post",
+        channel="The channel to post the message in",
+        time="The time to post the message at (format %Y/%m/%d %H:%M)",
+        tz="The timezone of your timestamp, default UTC (UTC = 0000, CET = 0100, CEST = 0200)",
+        message="The message to post, if not provided a popup window will be opened to type a message",
     )
+    @app_commands.rename(tz="timezone")
     async def schedule_message(
         self,
         ia: Interaction,
         channel: TextChannel,
         time: str,
+        tz: Optional[str] = "0000",
         message: Optional[str] = None,
     ):
         try:
-            parsed = datetime.strptime(time, "%Y/%m/%d %H:%M")
+            parsed_time = datetime.strptime(time + f"+{tz}", "%Y/%m/%d %H:%M%z")
         except ValueError:
             return await ia.response.send_message(
                 f"Invalid timestamp, it should be in the format `%Y/%m/%d %H:%M`, found {time}",
                 ephemeral=True,
             )
 
-        self.loop.create_task(self.send_delayed_message(channel, parsed, message))
+        if message is None:
+            return await ia.response.send_modal(
+                MessagePrompt(self.loop, channel, parsed_time)
+            )
 
-        return await ia.response.send_message(f"message scheduled for {parsed}")
+        try:
+            delay = schedule_message(self.loop, channel, parsed_time, message)
+        except ExpiredTimestamp as err:
+            return await ia.response.send_message(err.info)
+
+        return await ia.response.send_message(
+            f"Message scheduled to be sent in {delay}, at {parsed_time}"
+        )
 
 
 async def setup(bot: Bot):
