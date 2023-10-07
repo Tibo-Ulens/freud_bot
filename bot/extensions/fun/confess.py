3c115ca62a4707a2ef187a9b88efe3672ed44024
Tibo Ulens <ulens.tibo@gmail.com>
2023-10-07 21:43:12 +0200

    feat(bot): add extreme confessions + refactor code

--- bot/extensions/fun/confess.py
+++ bot/extensions/fun/confess.py
@@ -1,4 +1,5 @@
 import random
+from enum import Enum
 
 import discord
 from discord import (
@@ -22,30 +23,65 @@ from bot.decorators import (
 from bot.extensions import ErrorHandledCog
 
 
-async def send_pending_confession(
-    confession: str,
-    confession_channel: TextChannel,
-    approval_channel: TextChannel,
-    russian_roulette_user: Member = None,
-):
-    confession_embed = Embed(
-        colour=Colour.from_rgb(255, 255, 0),
-        title="🔫 Russian Roulette Confession 🔫"
-        if russian_roulette_user
-        else "🥷 Anonymous Confession 🥷",
-        description=confession,
+class ConfessionType(Enum):
+    NORMAL = (None, "🥷 Anonymous Confession 🥷", "Confession sent")
+    RUSSIAN = (
+        1 / 6,
+        "🔫 Russian Roulette Confession 🔫",
+        "Russian roulette confession sent",
+    )
+    EXTREME = (
+        1 / 2,
+        "💥 Extreme Roulette Confession 💥",
+        "Extreme roulette confession sent",
     )
 
-    pending_view = PendingApprovalView(
-        confession=confession_embed,
-        confession_channel=confession_channel,
-        russian_roulette_user=russian_roulette_user,
-    )
+    @property
+    def chance(self):
+        return self.value[0]
 
-    pending_view.message = await approval_channel.send(
-        embed=confession_embed,
-        view=pending_view,
-    )
+    @property
+    def title(self):
+        return self.value[1]
+
+    @property
+    def success_msg(self):
+        return self.value[2]
+
+
+class Confession:
+    def __init__(
+        self,
+        confession: str,
+        poster: Member,
+        type_: ConfessionType,
+        confession_channel: TextChannel,
+        approval_channel: TextChannel,
+    ):
+        self.confession = confession
+        self.poster = poster
+        self.type_ = type_
+        self.confession_channel = confession_channel
+        self.approval_channel = approval_channel
+
+    async def send_pending(self):
+        pending_embed = Embed(
+            colour=Colour.from_rgb(255, 255, 0),
+            title=self.type_.title,
+            description=self.confession,
+        )
+
+        pending_view = PendingApprovalView(
+            confession=pending_embed,
+            confession_channel=self.confession_channel,
+            poster=self.poster,
+            chance=self.type_.chance,
+        )
+
+        pending_view.message = await self.approval_channel.send(
+            embed=pending_embed,
+            view=pending_view,
+        )
 
 
 class PendingApprovalView(View):
@@ -53,12 +89,14 @@ class PendingApprovalView(View):
         self,
         confession: Embed,
         confession_channel: TextChannel,
-        russian_roulette_user: Member = None,
+        poster: Member,
+        chance: None | float,
     ):
         super().__init__(timeout=None)
         self.confession = confession
         self.confession_channel = confession_channel
-        self.russian_roulette_user = russian_roulette_user
+        self.poster = poster
+        self.chance = chance
 
     @discord.ui.button(label="✓", style=ButtonStyle.green)
     async def approve(self, ia: Interaction, _btn: Button):
@@ -66,10 +104,8 @@ class PendingApprovalView(View):
 
         actual_confession.colour = Colour.random()
 
-        if self.russian_roulette_user is not None and random.random() <= 1 / 6:
-            actual_confession.add_field(
-                name="Sent By", value=self.russian_roulette_user.mention
-            )
+        if self.chance is not None and random.random() <= self.chance:
+            actual_confession.add_field(name="Sent By", value=self.poster.mention)
 
         await self.confession_channel.send(embed=actual_confession)
 
@@ -93,13 +129,12 @@ class PendingApprovalView(View):
 
 
 class Confess(ErrorHandledCog):
-    @app_commands.command(name="confess", description="send an anonymous confession")
-    @app_commands.describe(confession="The confession you want to post")
-    @app_commands.guild_only()
-    @check_has_config_option("confession_approval_channel")
-    @check_has_config_option("confession_channel")
-    @check_user_is_verified()
-    async def confess(self, ia: Interaction, confession: str):
+    confess_group = app_commands.Group(
+        name="confess", description="Confession related commands", guild_only=True
+    )
+
+    @staticmethod
+    async def confess_inner(ia: Interaction, confession: str, type_: ConfessionType):
         config = await Config.get(ia.guild_id)
         approval_channel = discord.utils.get(
             ia.guild.channels, id=config.confession_approval_channel
@@ -108,38 +143,51 @@ class Confess(ErrorHandledCog):
             ia.guild.channels, id=config.confession_channel
         )
 
-        await send_pending_confession(confession, confession_channel, approval_channel)
+        confession_wrapper = Confession(
+            confession=confession,
+            poster=ia.user,
+            type_=type_,
+            confession_channel=confession_channel,
+            approval_channel=approval_channel,
+        )
 
-        await ia.response.send_message(content="Confession sent", ephemeral=True)
+        await confession_wrapper.send_pending()
 
-    @app_commands.command(
-        name="russianconfess",
-        description="send a confession with a 1 in 6 chance of not being anonymous",
+        await ia.response.send_message(
+            content=confession_wrapper.type_.success_msg, ephemeral=True
+        )
+
+    @confess_group.command(
+        name="normal", description="send a normal, anonymous confession"
     )
     @app_commands.describe(confession="The confession you want to post")
-    @app_commands.guild_only()
+    @check_has_config_option("confession_approval_channel")
+    @check_has_config_option("confession_channel")
+    @check_user_is_verified()
+    async def normal_confess(self, ia: Interaction, confession: str):
+        await self.confess_inner(ia, confession, ConfessionType.NORMAL)
+
+    @confess_group.command(
+        name="russian",
+        description="send a russian roulette confession with a 1 in 6 chance of not being anonymous",
+    )
+    @app_commands.describe(confession="The confession you want to post")
     @check_has_config_option("confession_approval_channel")
     @check_has_config_option("confession_channel")
     @check_user_is_verified()
     async def russian_confess(self, ia: Interaction, confession: str):
-        config = await Config.get(ia.guild_id)
-        approval_channel = discord.utils.get(
-            ia.guild.channels, id=config.confession_approval_channel
-        )
-        confession_channel = discord.utils.get(
-            ia.guild.channels, id=config.confession_channel
-        )
+        await self.confess_inner(ia, confession, ConfessionType.RUSSIAN)
 
-        await send_pending_confession(
-            confession,
-            confession_channel,
-            approval_channel,
-            russian_roulette_user=ia.user,
-        )
-
-        await ia.response.send_message(
-            content="Russian roulette confession sent", ephemeral=True
-        )
+    @confess_group.command(
+        name="extreme",
+        description="send an extreme roulette confession with a 1 in 2 chance of not being anonymous",
+    )
+    @app_commands.describe(confession="The confession you want to post")
+    @check_has_config_option("confession_approval_channel")
+    @check_has_config_option("confession_channel")
+    @check_user_is_verified()
+    async def russian_confess(self, ia: Interaction, confession: str):
+        await self.confess_inner(ia, confession, ConfessionType.EXTREME)
 
 
 async def setup(bot: Bot):
